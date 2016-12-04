c0154f63ebbb0e5fad8392b4660bbbf5f599c0d7
Craig Palermo <craig.palermo@opmbx.org>
2016-12-04 15:24:18 -0500

    Added test command, only for coinbase

--- clients/coinbase.py
+++ clients/coinbase.py
@@ -4,6 +4,26 @@ from settings import COINBASE_API_KEY, COINBASE_API_SECRET
 client = Client(COINBASE_API_KEY, COINBASE_API_SECRET)
 
 
+def _get_sell_price():
+    return client.get_sell_price(currency_pair='BTC-USD')
+
+
 def get_bitcoin_sell_price():
-    response = client.get_sell_price(currency_pair='BTC-USD')
+    response = _get_sell_price()
     return float(response["amount"]), response["currency"]
+
+
+def test():
+    message = "Testing connection to Coinbase API: "
+
+    try:
+        response = _get_sell_price()
+
+        if "amount" in response and "errors" not in response:
+            status = "success"
+        else:
+            status = "failure"
+    except Exception:
+        status = "failure"
+
+    print("".join([message, status.upper()]))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from price_alerter import PriceAlerter
-from clients.coinbase import get_bitcoin_sell_price
+from clients import coinbase as coinbase_client
 
 import argparse
 
@@ -10,14 +10,13 @@ import argparse
 parser = argparse.ArgumentParser(description='Send an SMS text message when Bitcoin reaches a certain value')
 parser.add_argument('--max', dest='max_limit', nargs=1, help='Alert when price is GREATER THAN max')
 parser.add_argument('--min', dest='min_limit', nargs=1, help='Alert when price is LESS THAN max')
-parser.add_argument('--phone', dest='recipient_phone', nargs=1, help='Recipient phone number for SMS alerts',
-                    required=True)
+parser.add_argument('--phone', dest='recipient_phone', nargs=1, help='Recipient phone number for SMS alerts')
+parser.add_argument('--test', action='store_true')
 
 
 def main():
     args = parser.parse_args()
     min_limit, max_limit = args.min_limit, args.max_limit
-    recipient_phone = args.recipient_phone[0]
 
     if min is not None or max is not None:
         min_limit = float(min_limit[0]) if min_limit is not None else 0
@@ -26,12 +25,19 @@ def main():
         if max_limit <= min_limit:
             print("Max must be greater than min limit. Exiting.")
             return
+        elif not args.recipient_phone and not args.test:
+            print("No recipient phone number given. Exiting.")
+            return
+        elif args.test:
+            coinbase_client.test()
+            return
 
+        recipient_phone = args.recipient_phone[0]
         alerter = PriceAlerter(recipient_phone, min_limit, max_limit)
         print("Bitcoin price alert now running. Min: {:.2f}, Max: {:.2f}".format(min_limit, max_limit))
 
         # Display current sell price
-        current_sell_price, currency = get_bitcoin_sell_price()
+        current_sell_price, currency = coinbase_client.get_bitcoin_sell_price()
         print("Current BTC sell price: {:.2f} {}".format(current_sell_price, currency))
 
         # Create blocking scheduler to run on interval
